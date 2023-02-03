1071e650d2b5af41c201db65e75c2a37f614b0ba
AxelRodriguez1328 <axelrodriguez201575@gmail.com>
2023-02-03 13:55:13 -0700

    Ejercicio1
    Ejercicio terminado

---
+++ Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Ejercicio1
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1TextBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(133, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite un numero";
+            // 
+            // Numero1TextBox1
+            // 
+            this.Numero1TextBox1.BackColor = System.Drawing.Color.White;
+            this.Numero1TextBox1.Location = new System.Drawing.Point(285, 124);
+            this.Numero1TextBox1.Name = "Numero1TextBox1";
+            this.Numero1TextBox1.Size = new System.Drawing.Size(123, 20);
+            this.Numero1TextBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(283, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numero Par o Impar, Positivo o Negativo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Numero1TextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
+            this.Text = "Ejercicio1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1TextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(Numero1TextBox1.Text);
+            imparopar(numero);
+            negativoopositivo(numero);
+        }
+
+        private void imparopar (int numero) 
+        {
+            int sobrante = numero % 2;
+            if (sobrante == 0)
+            {
+                MessageBox.Show("El numero es Par", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El numero es Impar", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void negativoopositivo(int numero)
+        {
+            if (numero >= 1) 
+            {
+                MessageBox.Show("El numero es positivo","Informacion",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El numero es Negativo", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        }
+}
+
+
